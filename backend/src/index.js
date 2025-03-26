85d946307eafbdcbfb3dbd28e2e88f4e1a21e1ba
wobahthebest <vebdevfrontend@gmail.com>
2025-03-26 10:32:29 +0500

    demo version is compleatelly done

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "../lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser({ limit: "50mb", extended: true }));
@@ -20,6 +24,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(process.env.PORT, () => {
   console.log("the port is running on port: ", process.env.PORT);
